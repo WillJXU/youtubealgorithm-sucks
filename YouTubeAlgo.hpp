354f01465ea5f8b42d0dd3d9ef180664da1f3dc8
ringkun <ringkun12@gmail.com>
2019-04-10 19:53:15 -0600

    Added comments and a new function "isStopWord"

--- YouTubeAlgo.hpp
+++ YouTubeAlgo.hpp
@@ -33,16 +33,17 @@ class searchalgo
   searchalgo(std::string filename);   //constructor, builds bst
   ~searchalgo();    //deconstructor
   string getkeywords(std::string videotitle, std::string keywordTab[]);
-  videoll *getsuggestions(vector <std::string> keywordTab);
+  videoll *getsuggestions(vector <std::string> keywordTab); // Goes through the bst and finds best suggestions
   //Helpers
-  videoll* getVideoPointer(std::string title);
-  searchalgo::getKeyWordPoint(keywordbst* root ,std::string keyword)  
-  void getstopwords();  //constructs array of stop words
+  videoll* getVideoPointer(std::string title); // Returns the pointer of a videoll through title
+  searchalgo::getKeyWordPoint(keywordbst* root ,std::string keyword) // Returns pointer of a keyword node
+  void getstopwords(std::string filename);  //constructs hashtable of stop words
+  bool isStopWord(std::string word); // Checks if the word is a stop work
   private:
-  int videocount;
-  int videowatched;
-  vector <videoll> watchedvideo;
-  int keywordcount;
+  int videocount; // Total number of video in archive
+  int videowatched; // Number of videos seen by user
+  vector <videoll> watchedvideo; //Vector of the videos seen by user in the session
+  int keywordcount; // Number of Keywords
   keywordsbst *root;    //bst root
   videoll* VideoHash[26];
   ignoreWord* stopWordHash[26];
