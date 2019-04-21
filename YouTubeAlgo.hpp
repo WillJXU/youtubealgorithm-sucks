32b0718282601468d02f80cc24450714aace1b67
ringkun <ringkun12@gmail.com>
2019-04-21 04:50:46 -0600

    Update YouTubeAlgo.hpp

--- YouTubeAlgo.hpp
+++ YouTubeAlgo.hpp
@@ -1,11 +1,11 @@
 #ifndef YOUTUBEALGO_HPP
 #define YOUTUBEALGO_HPP
 #include <iostream>
-#include <fstream>
 #include <sstream>
 #include <string>
-#include <vector>
-
+#include <fstream>
+#include<vector>
+#include <math.h>
 using namespace std;
 
 struct videoll{
@@ -16,7 +16,7 @@ struct videoll{
     int duration;
     int views;
     string ID;
-    struct videoll *next = NULL;
+    struct videoll *next;
 
     videoll(){} // default constructor
 
@@ -24,8 +24,17 @@ struct videoll{
     videoll(string c, string d, string t, float r, int w, int v, string i) : creator(c),
       date(d), title(t), likeratio(r), duration(w), views(v), ID(i), next(NULL) {}
 };
-
-struct keywordsbst{
+struct vid{
+	std::string title = "";
+	std::string keywordlist = "";
+	int count = 0;
+	vid* n = NULL;
+	vid* par = NULL;
+	float recScore = 0;
+	videoll* refNode = NULL;
+};
+struct keywordsbst
+{
     string keyword; //node keyword
     keywordsbst *parent = NULL;
     keywordsbst *l = NULL;
@@ -34,37 +43,87 @@ struct keywordsbst{
 };
 
 struct ignoreWord{
-    string word = "";
+    std::string word = "";
     ignoreWord* next = NULL;
 };
 
-struct videokeywords{
+struct videokeywords
+{
     string key;
 };
 
-class searchalgo{
-  public:
-    //Ben
-    searchalgo(std::string filename);   //constructor, builds bst
-    void addkeyword(string keyword);
-    int totalvideokeywords(string videotitle);
-    videokeywords* getkeywords(string videotitle, int videotitlewords);
-    //Helpers
-    bool findmatching(keywordsbst *n, string keyword);
-    void getstopwords(string filename);
-    bool isStopWord(string s);
-    //Debug ONLY
-    void dispInOrd(keywordsbst *n);     //DEBUG ONLY
-    void printll(videoll *n);
-  private:
-    int videocount;
-    int videowatched;
-    videokeywords* keyword; //pointer to the table array
-    //vector <videoll> watchedvideo;
-    int keywordcount;
-    keywordsbst *root;    //bst root
-    bool match;
-    videoll* VideoHash[26];
-    ignoreWord* stopWordHash[26];
+class searchalgo
+{
+	public:
+	//BEN
+	searchalgo(std::string filename);   //constructor, reads data from file
+	~searchalgo();    //deconstructor
+	void addkeyword(string keyword);
+	int totalvideokeywords(string videotitle); // calculates size of video keywords array
+	videokeywords* getkeywords(string videotitle, int videotitlewords);
+	//WILLIAM
+	videoll *getsuggestions(vector <std::string> keywordTab); // Goes through the bst and finds best suggestions
+	//Helpers
+	videoll* getVideoPointer(std::string title); // Returns the pointer of a videoll through title
+	keywordsbst* getKeyWordPoint(std::string keyword); // Returns pointer of a keyword node
+	bool findmatching(keywordsbst *n, string keyword); //Checks if keyword is already in the tree
+	void getstopwords(std::string filename);  //constructs hashtable of stop words
+	bool isStopWord(std::string word); // Checks if the word is a stop work
+	std::string watchVidFromQueue(int i){
+		std::string s;
+		vid* check = queue;
+		if (i == 1){
+			s = queue->title;
+			queue = queue->n;
+			return s;
+		}
+		while (i>1) {
+			check = check->n;
+			i--;
+		}
+		s = check->title;
+		if (i == vidrecco){
+			check->par->n = NULL;
+			return s;
+		}
+		check->par->n = check->n;
+		return s;
+	}
+	void printVideoDetail(vid* check);
+	void readDetailFromQueue(int i){
+		vid* check = queue;
+		while (i>1) {
+			check = check->n;
+			i--;
+		}
+		printVideoDetail(check);
+	}
+	void printallinfo();
+	int getrecconum(){
+		return vidrecco;
+	}
+	keywordsbst* getRoot(){
+	return root;
+	}
+	void printmenu();
+	//DEBUG ONLY
+
+	void dispInOrd(keywordsbst *n); //prints BST, calls to printll at each node
+	void printll(videoll *n);
+	void setNumofRec(int n){
+		vidrecco = n;
+	}
+	private:
+	vid* queue = NULL;
+	int vidrecco = 10;
+	int videocount; // Total number of video in archive
+	int videowatched; // Number of videos seen by user
+	videokeywords* keyword; //pointer to the array of video keywords
+	vector <videoll> watchedvideo; //Vector of the videos seen by user in the session
+	int keywordcount; // Number of Keywords
+	keywordsbst *root;    //bst root
+	bool match;
+	vid* VideoHash[57];
+	ignoreWord* stopWordHash[26];
 };
 #endif
